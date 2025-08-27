fbe314027b50ada61ccafe3732bb4c60d5b9149d
PleitesGC-12 <primezero04@gmail.com>
2025-08-26 20:58:13 -0600

    Agregado el codigo al main para poder mostrar, arrancar , apagar y pintar el coche

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-      
-        // Crear un objeto de tipo Coche
+        Coche coche1 = new Coche("Lamborghini " , "Urus" , "Naranja");
+        Coche coche2 = new Coche("Ferrari " , "250 GTO" , "Amarillo");
+        Coche coche3 = new Coche("Ford " , "Mustang" , "Rojo");
+
+        coche1.describir();
+        coche2.describir();
+        coche3.describir();
+
+        //coloreando
+        coche1.pintar("Rojo");
+        coche2.pintar("Morado");
+        coche3.pintar("Verde");
+
+        coche1.arrancar();
+        coche2.arrancar();
+        coche3.arrancar();
+
+        // Describir luego de arrancar
+        coche1.describir();
+        coche2.describir();
+        coche3.describir();
+
+        //describir luego de colorear
+        coche1.describir();
+        coche2.describir();
+        coche3.describir();
+
+        // Describir lluego de apagar
+        coche1.apagar();
+        coche2.apagar();
+        coche3.apagar();
 
 
-         // Mostrar información inicial del coche
 
-        // Encender el coche
-
-                // Cambiar el color del coche
-
-                // Apagar el coche
-     
-        }
     }
+}
